f4fcbad226ffa68aa9633a7681c3a6d0fea69920
Jibreel Hamid-Diaz <shuckle1028@gmail.com>
2018-01-25 00:04:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function dec(n){
   return n -= 1
 }
 function makeInt(n){
-  return parseInt('749', 10)
+  return parseInt('114.1000010', 10)
 }
 function preserveDecimal(n){
   return parseFloat('2.222')
